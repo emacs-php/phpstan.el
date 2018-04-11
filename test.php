bc56031795df97a1dadf15b85712915ac22461d8
USAMI Kenta <tadsan@zonu.me>
2018-04-12 01:06:00 +0900

    Add test files

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+
+foo();
+f();
+foo();
+
+echo Fooo;
+echo FOO;
+
+// Local Variables:
+// phpstan-configure-file: (root . "tests/phpstan.neon")
+// phpstan-level: 7
+// End:
---
+++ tests/bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+
+const FOO = 'Foo';
+
+function foo() {}
