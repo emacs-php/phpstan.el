21cf970d3b43478e035e59199fbae02875a41d68
USAMI Kenta <tadsan@zonu.me>
2018-04-13 03:05:07 +0900

    Modify doc comment and README

--- phpstan.el
+++ phpstan.el
@@ -50,7 +50,19 @@
 
 ;;;###autoload
 (progn
-  (defvar phpstan-working-dir nil)
+  (defvar phpstan-working-dir nil
+    "Path to working directory of PHPStan.
+
+*NOTICE*: This is different from the project root.
+
+STRING
+     Absolute path to `phpstan' working directory.
+
+`(root . STRING)'
+     Relative path to `phpstan' working directory from project root directory.
+
+NIL
+     Use (php-project-get-root-dir) as working directory.")
   (make-variable-buffer-local 'phpstan-working-dir)
   (put 'phpstan-working-dir 'safe-local-variable
        #'(lambda (v) (if (consp v)
@@ -59,7 +71,17 @@
 
 ;;;###autoload
 (progn
-  (defvar phpstan-config-file nil)
+  (defvar phpstan-config-file nil
+    "Path to project specific configuration file of PHPStan.
+
+STRING
+     Absolute path to `phpstan' configuration file.
+
+`(root . STRING)'
+     Relative path to `phpstan' configuration file from project root directory.
+
+NIL
+     Search phpstan.neon(.dist) in (phpstan-get-working-dir).")
   (make-variable-buffer-local 'phpstan-config-file)
   (put 'phpstan-config-file 'safe-local-variable
        #'(lambda (v) (if (consp v)
@@ -68,7 +90,14 @@
 
 ;;;###autoload
 (progn
-  (defvar phpstan-level "0")
+  (defvar phpstan-level "0"
+    "Rule level of PHPStan.
+
+INTEGER or STRING
+     Number of PHPStan rule level.
+
+max
+     The highest of PHPStan rule level.")
   (make-variable-buffer-local 'phpstan-level)
   (put 'phpstan-level 'safe-local-variable
        #'(lambda (v) (or (null v)
@@ -118,18 +147,7 @@ NIL
 
 ;; Functions:
 (defun phpstan-get-working-dir ()
-  "Return working directory of PHPStan.
-
-This is different from the project root.
-
-STRING
-     Absolute path to `phpstan' working directory.
-
-`(root . STRING)'
-     Relative path to `phpstan' working directory.
-
-NIL
-     Use (php-project-get-root-dir) as working directory."
+  "Return path to working directory of PHPStan."
   (if (and phpstan-working-dir (consp phpstan-working-dir) (eq 'root (car phpstan-working-dir)))
       (expand-file-name (cdr phpstan-working-dir) (php-project-get-root-dir))
     (php-project-get-root-dir)))
