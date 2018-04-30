7edba83723ba0f1da567f44ff2cd720576275835
USAMI Kenta <tadsan@zonu.me>
2018-04-30 19:57:50 +0900

    Add describe header

--- flycheck-phpstan.el
+++ flycheck-phpstan.el
@@ -25,6 +25,18 @@
 ;;; Commentary:
 
 ;; Flycheck integration for PHPStan.
+;;
+;; Put the following into your .emacs file (~/.emacs.d/init.el)
+;;
+;;     (defun my-php-mode-hook ()
+;;       "My PHP-mode hook."
+;;       (require 'flycheck-phpstan)
+;;       (flycheck-mode t)
+;;       (flycheck-select-checker 'phpstan))
+;;
+;;     (add-hook 'php-mode-hook 'my-php-mode-hook)
+;;
+
 
 ;;; Code:
 (require 'flycheck)
