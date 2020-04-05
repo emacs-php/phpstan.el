9345480715b12fe1346708e714ce8e12ce3a9ae2
USAMI Kenta <tadsan@zonu.me>
2020-04-05 20:19:22 +0900

    Add require php-mode 1.22.3

--- phpstan.el
+++ phpstan.el
@@ -7,7 +7,7 @@
 ;; Version: 0.4.0
 ;; Keywords: tools, php
 ;; Homepage: https://github.com/emacs-php/phpstan.el
-;; Package-Requires: ((emacs "24.3"))
+;; Package-Requires: ((emacs "24.3") (php-mode "1.22.3"))
 ;; License: GPL-3.0-or-later
 
 ;; This program is free software; you can redistribute it and/or modify
