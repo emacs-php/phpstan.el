6d77f9a69cbaf42cfb3b671ed133e4b878745738
USAMI Kenta <tadsan@zonu.me>
2026-02-16 23:34:19 +0900

    Improve phpstan-hover-tree-fetcher.php

--- php/phpstan-hover-tree-fetcher.php
+++ php/phpstan-hover-tree-fetcher.php
@@ -16,10 +16,19 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 use PhpParser\Node;
 use PhpParser\Node\Expr\ArrowFunction;
 use PhpParser\Node\Expr\Assign;
+use PhpParser\Node\Expr\Closure;
+use PhpParser\Node\Expr\FuncCall;
+use PhpParser\Node\Expr\MethodCall;
 use PhpParser\Node\Expr\PropertyFetch;
+use PhpParser\Node\Expr\StaticCall;
 use PhpParser\Node\Expr\Variable;
+use PhpParser\Node\Expr\YieldFrom;
+use PhpParser\Node\Expr\Yield_;
 use PhpParser\Node\FunctionLike;
+use PhpParser\Node\Identifier;
+use PhpParser\Node\Name;
 use PhpParser\Node\Param;
+use PhpParser\Node\Stmt\Return_;
 use PHPStan\Analyser\Scope;
 use PHPStan\Collectors\Collector;
 use PHPStan\Node\CollectedDataNode;
@@ -32,67 +41,51 @@ use PHPStan\Type\ErrorType;
 use PHPStan\Type\Type;
 use PHPStan\Type\VerbosityLevel;
 
-class PHPStanVSCodeLogger {
-	public static function log(...$args) {
-		foreach ($args as $arg) {
-			print_r($arg);
-			print(" ");
-		}
-		print("\n");
-	}
-}
-
-class PHPStanVSCodeTreeFetcher implements Rule {
-	// Replaced at runtime with a tmp file
-	public const REPORTER_FILE = 'reported.json';
+class PHPStanEmacsHoverTreeFetcher implements Rule {
+	public const REPORTER_FILE = '__PHPSTAN_EMACS_HOVER_REPORT_FILE__';
 
 	public function getNodeType(): string {
 		return CollectedDataNode::class;
 	}
 
 	public static function binarySearch(&$array, $target) {
-    $left = 0;
-    $right = count($array) - 1;
+		$left = 0;
+		$right = count($array) - 1;
 
-    while ($left <= $right) {
+		while ($left <= $right) {
 			$mid = floor(($left + $right) / 2);
 
 			if ($array[$mid] <= $target) {
 				if ($mid == count($array) - 1 || $array[$mid + 1] > $target) {
-					return $mid; // Closest index that is less than or equal to the target
-				} else {
-					$left = $mid + 1;
+					return $mid;
 				}
+				$left = $mid + 1;
 			} else {
 				$right = $mid - 1;
 			}
-    }
+		}
 
-    return -1; // Target not found
-}
+		return -1;
+	}
 
 	/**
 	 * @param array<string, list<list<array{
 	 *   typeDescr: string,
 	 *   name: string,
+	 *   kind?: string,
 	 *   pos: array{
 	 *     start: int,
 	 *     end: int
 	 *   }
-	 * }>>> $nodeDatas
+	 * }>>> $fileDatas
 	 * @return array<string, list<array{
 	 *  typeDescr: string,
-	 * 	name: string,
-	 * 	pos: array{
-	 * 		start: array{
-	 * 			line: int,
-	 * 			char: int
-	 * 		},
-	 * 		end: array{
-	 * 			line: int,
-	 * 			char: int
-	 * 		}
-	 * 	}
+	 *  name: string,
+	 *  kind?: string,
+	 *  pos: array{
+	 *    start: array{line: int, char: int},
+	 *    end: array{line: int, char: int}
+	 *  }
 	 * }>>
 	 */
 	public static function convertCharIndicesToPositions(array $fileDatas): array {
@@ -102,23 +95,25 @@ class PHPStanVSCodeTreeFetcher implements Rule {
 				continue;
 			}
 			$file = file_get_contents($filePath);
+			if (!is_string($file)) {
+				continue;
+			}
 			$results[$filePath] = [];
 
-			$lineOffsets = [0]; // Initialize with the first line starting at index 0
+			$lineOffsets = [0];
 			for ($i = 0; $i < strlen($file); $i++) {
 				if ($file[$i] === "\n") {
-					$lineOffsets[] = $i + 1; // Add 1 to include the newline character
+					$lineOffsets[] = $i + 1;
 				}
 			}
 
-			// Use binary search to find the line number efficiently
 			$findPos = static function (int $filePos) use ($lineOffsets) {
 				$line = self::binarySearch($lineOffsets, $filePos);
 				$lineStart = $lineOffsets[$line];
 				$char = $filePos - $lineStart;
 				return [
 					'line' => $line,
-					'char' => $char
+					'char' => $char,
 				];
 			};
 
@@ -128,13 +123,14 @@ class PHPStanVSCodeTreeFetcher implements Rule {
 					$results[$filePath][] = [
 						'typeDescr' => $datum['typeDescr'],
 						'name' => $datum['name'],
+						'kind' => $datum['kind'] ?? null,
 						'pos' => [
 							'start' => $findPos($datum['pos']['start']),
 							'end' => [
 								'line' => $endPos['line'],
-								'char' => $endPos['char']
-							]
-						]
+								'char' => $endPos['char'],
+							],
+						],
 					];
 				}
 			}
@@ -145,7 +141,7 @@ class PHPStanVSCodeTreeFetcher implements Rule {
 
 	/** @param CollectedDataNode $node */
 	public function processNode(Node $node, Scope $scope): array {
-		$collectedData = $node->get(PHPStanVSCodeTreeFetcherCollector::class);
+		$collectedData = $node->get(PHPStanEmacsHoverTreeFetcherCollector::class);
 		file_put_contents(self::REPORTER_FILE, json_encode(self::convertCharIndicesToPositions($collectedData)));
 		return [];
 	}
@@ -155,22 +151,18 @@ class PHPStanVSCodeTreeFetcher implements Rule {
  * @phpstan-type CollectedData array{
  *   typeDescr: string,
  *   name: string,
- *   pos: array{
- *     start: int,
- *     end: int
- *   }
+ *   pos: array{start: int, end: int}
  * }
  * @implements Collector<Node, list<CollectedData>>
  */
-class PHPStanVSCodeTreeFetcherCollector implements Collector {
-	/** @var list<array{ClosureType, list<array{startPos: int, endPos: int, isUsed: false, closureNode: Closure|ArrowFunction}}}>> */
+class PHPStanEmacsHoverTreeFetcherCollector implements Collector {
+	/** @var list<array{ClosureType, list<array{startPos: int, endPos: int, isUsed: false, closureNode: Closure|ArrowFunction}>}> */
 	private $closureTypeToNode = [];
 
 	/**
-	 * @return ?list<array{startPos: int, endPos: int, isUsed: false, closureNode: Closure|ArrowFunction}}}>
+	 * @return ?list<array{startPos: int, endPos: int, isUsed: false, closureNode: Closure|ArrowFunction}>
 	 */
-	protected function getClosuresFromScope(Scope $scope): ?array
-	{
+	protected function getClosuresFromScope(Scope $scope): ?array {
 		$anonymousFunctionReflection = $scope->getAnonymousFunctionReflection();
 		if ($anonymousFunctionReflection) {
 			foreach ($this->closureTypeToNode as $closureTypeToNode) {
@@ -178,25 +170,21 @@ class PHPStanVSCodeTreeFetcherCollector implements Collector {
 				if ($anonymousFunctionReflection !== $closureType) {
 					continue;
 				}
-
 				return $closureClosures;
 			}
 		}
 		return null;
 	}
 
-	protected function processClosures(Node $node, Scope $scope): void
-	{
+	protected function processClosures(Node $node, Scope $scope): void {
 		if ($node instanceof Closure || $node instanceof ArrowFunction) {
-			// We grab the type as well as the node and connect the two so that later
-			// callers inside this closure can resolve to the node from the type.
 			$closureType = $scope->getType($node);
 			$existingClosures = $this->getClosuresFromScope($scope) ?? [];
 			$existingClosures[] = [
 				'startPos' => $node->getStartFilePos(),
 				'endPos' => $node->getEndFilePos() + 1,
 				'isUsed' => false,
-				'closureNode' => $node
+				'closureNode' => $node,
 			];
 			$this->closureTypeToNode[] = [$closureType, $existingClosures];
 		}
@@ -205,14 +193,12 @@ class PHPStanVSCodeTreeFetcherCollector implements Collector {
 	/** @var list<string> */
 	private $visitedFunctions = [];
 
-	/**
-	 * @return list<CollectedData>
-	 */
-	private function _processFunction(Scope $scope): array {
+	/** @return list<CollectedData> */
+	private function processFunctionScope(Scope $scope): array {
 		$functionKey = implode('.', [
 			$scope->getFile(),
 			$scope->getClassReflection() ? $scope->getClassReflection()->getName() : null,
-			$scope->getFunctionName()
+			$scope->getFunctionName(),
 		]);
 		if (in_array($functionKey, $this->visitedFunctions, true)) {
 			return [];
@@ -225,72 +211,66 @@ class PHPStanVSCodeTreeFetcherCollector implements Collector {
 			return [];
 		}
 
-		$reflectionClass = new ReflectionClass(PhpMethodFromParserNodeReflection ::class);
+		$reflectionClass = new ReflectionClass(PhpMethodFromParserNodeReflection::class);
 		$reflectionMethod = $reflectionClass->getMethod('getFunctionLike');
 		$reflectionMethod->setAccessible(true);
 		$fnLike = $reflectionMethod->invoke($function);
 		return $this->onFunction($fnLike, $function);
 	}
 
+	/** @var list<string> */
+	private $visitedClosures = [];
+
 	/**
-	 * @param list<array{startPos: int, endPos: int, isUsed: false, closureNode: Closure|ArrowFunction}}}> $closures
+	 * @param list<array{startPos: int, endPos: int, isUsed: false, closureNode: Closure|ArrowFunction}> $closures
 	 * @return list<CollectedData>
 	 */
-	private function _processClosure(Scope $scope, array $closures): array {
+	private function processClosureScope(Scope $scope, array $closures): array {
 		$functionKey = implode('.', [
 			$scope->getFile(),
 			$scope->getClassReflection() ? $scope->getClassReflection()->getName() : null,
-			json_encode($closures)
+			json_encode($closures),
 		]);
-		if (in_array($functionKey, $this->visitedMethods, true)) {
+		if (in_array($functionKey, $this->visitedClosures, true)) {
 			return [];
 		}
-		$this->visitedMethods[] = $functionKey;
+		$this->visitedClosures[] = $functionKey;
 
 		$lastClosure = end($closures);
-		/** @var Closure|ArrowFunction */
+		/** @var Closure|ArrowFunction $lastClosureNode */
 		$lastClosureNode = $lastClosure['closureNode'];
 		$fnReflection = $scope->getAnonymousFunctionReflection();
 		assert($fnReflection !== null);
 		return $this->onClosure($lastClosureNode, $fnReflection);
 	}
 
-	/**
-	 * @return list<CollectedData>
-	 */
-	public function processFunctionTrackings(Node $node, Scope $scope): array
-	{
-		/** @var list<CollectedData> */
+	/** @return list<CollectedData> */
+	public function processFunctionTrackings(Node $node, Scope $scope): array {
+		/** @var list<CollectedData> $data */
 		$data = [];
 		$this->processClosures($node, $scope);
+
 		if ($scope->getFunctionName()) {
-			$data = array_merge($data, $this->_processFunction($scope));
+			$data = array_merge($data, $this->processFunctionScope($scope));
 		}
 
 		$closures = $this->getClosuresFromScope($scope);
 		if ($closures) {
-			$data = array_merge($data, $this->_processClosure($scope, $closures));
+			$data = array_merge($data, $this->processClosureScope($scope, $closures));
 		}
+
 		return $data;
 	}
 
-	/** @var list<string> */
-	private $visitedMethods = [];
-
-	public function getNodeType(): string
-	{
+	public function getNodeType(): string {
 		return Node::class;
 	}
 
-	/**
-	 * @return ?CollectedData
-	 */
-	private function processNodeWithType($node, Type $type): ?array
-	{
+	/** @return ?CollectedData */
+	private function processNodeWithType($node, Type $type): ?array {
 		$varName = $node instanceof Variable ? $node->name : $node->name->name;
 		$typeDescr = $type->describe(VerbosityLevel::precise());
 		if (!is_string($varName)) {
-			// Not a plain string, can't handle this
 			return null;
 		}
 
@@ -301,17 +281,146 @@ class PHPStanVSCodeTreeFetcherCollector implements Collector {
 		return [
 			'typeDescr' => $typeDescr,
 			'name' => $varName,
+			'kind' => 'variable',
 			'pos' => [
-				// Include `$` for variables
 				'start' => $node->getStartFilePos() - ($node instanceof Variable ? 1 : 0),
-				'end' => $node->getEndFilePos() + 1
-			]
+				'end' => $node->getEndFilePos() + 1,
+			],
 		];
 	}
 
-	/**
-	 * @param list<array{startPos: int, endPos: int, isUsed: false, closureNode: Closure|ArrowFunction}}}> $closures
-	 */
+	/** @return ?CollectedData */
+	private function processReturnNode(Return_ $node, Scope $scope): ?array {
+		$type = null;
+		if ($node->expr !== null) {
+			$type = $scope->getType($node->expr);
+		} else {
+			$function = $scope->getFunction();
+			if ($function) {
+				$type = $function->getReturnType();
+			}
+		}
+
+		if (!$type || $type instanceof ErrorType) {
+			return null;
+		}
+		if ($node->getStartFilePos() === -1) {
+			return null;
+		}
+
+		$start = $node->getStartFilePos();
+		$end = $start + 6; // "return"
+		if ($node->getEndFilePos() !== -1) {
+			$end = min($end, $node->getEndFilePos() + 1);
+		}
+
+		return [
+			'typeDescr' => $type->describe(VerbosityLevel::precise()),
+			'name' => 'return',
+			'kind' => 'return',
+			'pos' => [
+				'start' => $start,
+				'end' => $end,
+			],
+		];
+	}
+
+	/** @return ?CollectedData */
+	private function processYieldNode(Yield_ $node, Scope $scope): ?array {
+		$type = null;
+		if ($node->value !== null) {
+			$type = $scope->getType($node->value);
+		}
+		if (!$type) {
+			$type = $scope->getType($node);
+		}
+		if (!$type || $type instanceof ErrorType) {
+			return null;
+		}
+		if ($node->getStartFilePos() === -1) {
+			return null;
+		}
+
+		$start = $node->getStartFilePos();
+		$end = $start + 5; // "yield"
+		if ($node->getEndFilePos() !== -1) {
+			$end = min($end, $node->getEndFilePos() + 1);
+		}
+
+		return [
+			'typeDescr' => $type->describe(VerbosityLevel::precise()),
+			'name' => 'yield',
+			'kind' => 'yield',
+			'pos' => [
+				'start' => $start,
+				'end' => $end,
+			],
+		];
+	}
+
+	/** @return ?CollectedData */
+	private function processYieldFromNode(YieldFrom $node, Scope $scope): ?array {
+		$type = $scope->getType($node->expr);
+		if (!$type || $type instanceof ErrorType) {
+			return null;
+		}
+		if ($node->getStartFilePos() === -1) {
+			return null;
+		}
+
+		$start = $node->getStartFilePos();
+		$end = $start + 10; // "yield from"
+		if ($node->getEndFilePos() !== -1) {
+			$end = min($end, $node->getEndFilePos() + 1);
+		}
+
+		return [
+			'typeDescr' => $type->describe(VerbosityLevel::precise()),
+			'name' => 'yield from',
+			'kind' => 'yield-from',
+			'pos' => [
+				'start' => $start,
+				'end' => $end,
+			],
+		];
+	}
+
+	/** @return ?CollectedData */
+	private function processCallNode(Node $nameNode, Type $type, string $name): ?array {
+		if ($type instanceof ErrorType) {
+			return null;
+		}
+		if ($nameNode->getStartFilePos() === -1 || $nameNode->getEndFilePos() === -1) {
+			return null;
+		}
+
+		return [
+			'typeDescr' => $type->describe(VerbosityLevel::precise()),
+			'name' => $name,
+			'kind' => 'call',
+			'pos' => [
+				'start' => $nameNode->getStartFilePos(),
+				'end' => $nameNode->getEndFilePos() + 1,
+			],
+		];
+	}
+
+	/** @return ?CollectedData */
+	private function processAssignNode(Assign $node, Scope $scope): ?array {
+		$assigned = $node->var;
+		if (!($assigned instanceof Variable || $assigned instanceof PropertyFetch)) {
+			return null;
+		}
+
+		$type = $scope->getType($node->expr);
+		if ($type instanceof ErrorType) {
+			return null;
+		}
+
+		return $this->processNodeWithType($assigned, $type);
+	}
+
+	/** @return list<CollectedData> */
 	protected function onClosure($node, ParametersAcceptor $type): array {
 		/** @var array<string, Param> */
 		$paramNodesByName = [];
@@ -329,7 +438,6 @@ class PHPStanVSCodeTreeFetcherCollector implements Collector {
 
 			$typeDescr = $parameter->getType()->describe(VerbosityLevel::precise());
 			if ($paramNode->getStartFilePos() === -1 || $paramNode->getEndFilePos() === -1) {
-				// Implicit parameter
 				continue;
 			}
 
@@ -338,15 +446,15 @@ class PHPStanVSCodeTreeFetcherCollector implements Collector {
 				'name' => $parameter->getName(),
 				'pos' => [
 					'start' => $paramNode->getStartFilePos(),
-					'end' => $paramNode->getEndFilePos() + 1
-				]
+					'end' => $paramNode->getEndFilePos() + 1,
+				],
 			];
 		}
 
 		return $data;
 	}
 
-	/** @var list<CollectedData> */
+	/** @return list<CollectedData> */
 	protected function onFunction(FunctionLike $node, PhpMethodFromParserNodeReflection $type): array {
 		/** @var list<CollectedData> $data */
 		$data = [];
@@ -365,18 +473,17 @@ class PHPStanVSCodeTreeFetcherCollector implements Collector {
 				}
 
 				$typeDescr = $parameter->getType()->describe(VerbosityLevel::precise());
-
 				if ($paramNode->getStartFilePos() === -1 || $paramNode->getEndFilePos() === -1) {
-					// Implicit parameter
 					continue;
 				}
+
 				$data[] = [
 					'typeDescr' => $typeDescr,
 					'name' => $parameter->getName(),
 					'pos' => [
 						'start' => $paramNode->getStartFilePos(),
-						'end' => $paramNode->getEndFilePos() + 1
-					]
+						'end' => $paramNode->getEndFilePos() + 1,
+					],
 				];
 			}
 		}
@@ -384,15 +491,14 @@ class PHPStanVSCodeTreeFetcherCollector implements Collector {
 		return $data;
 	}
 
-	/** @var list<CollectedData> */
-	public function processNode(Node $node, Scope $scope): ?array
-	{
+	/** @return ?list<CollectedData> */
+	public function processNode(Node $node, Scope $scope): ?array {
 		if ($scope->getTraitReflection()) {
 			return null;
 		}
+
 		/** @var list<CollectedData> $data */
 		$data = [];
-
 		$data = array_merge($data, $this->processFunctionTrackings($node, $scope));
 
 		if ($node instanceof InForeachNode) {
@@ -405,12 +511,41 @@ class PHPStanVSCodeTreeFetcherCollector implements Collector {
 					if ($nodeWithType) {
 						$data[] = $nodeWithType;
 					}
-				} else if ($valueVar && $valueVar instanceof Variable) {
+				}
+				if ($valueVar && $valueVar instanceof Variable) {
 					$nodeWithType = $this->processNodeWithType($valueVar, $exprType->getItemType());
 					if ($nodeWithType) {
 						$data[] = $nodeWithType;
 					}
 				}
+			}
+		}
+
+		if ($node instanceof Assign) {
+			$assignNodeData = $this->processAssignNode($node, $scope);
+			if ($assignNodeData) {
+				$data[] = $assignNodeData;
+			}
+		}
+
+		if ($node instanceof FuncCall && $node->name instanceof Name) {
+			$callNodeData = $this->processCallNode($node->name, $scope->getType($node), $node->name->toString());
+			if ($callNodeData) {
+				$data[] = $callNodeData;
+			}
+		}
+
+		if ($node instanceof MethodCall && $node->name instanceof Identifier) {
+			$callNodeData = $this->processCallNode($node->name, $scope->getType($node), $node->name->toString());
+			if ($callNodeData) {
+				$data[] = $callNodeData;
+			}
+		}
+
+		if ($node instanceof StaticCall && $node->name instanceof Identifier) {
+			$callNodeData = $this->processCallNode($node->name, $scope->getType($node), $node->name->toString());
+			if ($callNodeData) {
+				$data[] = $callNodeData;
 			}
 		}
 
@@ -425,6 +560,27 @@ class PHPStanVSCodeTreeFetcherCollector implements Collector {
 				if ($nodeWithType) {
 					$data[] = $nodeWithType;
 				}
+			}
+		}
+
+		if ($node instanceof Return_) {
+			$returnNodeData = $this->processReturnNode($node, $scope);
+			if ($returnNodeData) {
+				$data[] = $returnNodeData;
+			}
+		}
+
+		if ($node instanceof Yield_) {
+			$yieldNodeData = $this->processYieldNode($node, $scope);
+			if ($yieldNodeData) {
+				$data[] = $yieldNodeData;
+			}
+		}
+
+		if ($node instanceof YieldFrom) {
+			$yieldFromNodeData = $this->processYieldFromNode($node, $scope);
+			if ($yieldFromNodeData) {
+				$data[] = $yieldFromNodeData;
 			}
 		}
 
